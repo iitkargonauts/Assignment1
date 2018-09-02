3c07fb381a769bb5d40d9bb879aa29e952061c7f
iitkargonauts <42734813+iitkargonauts@users.noreply.github.com>
2018-09-02 23:49:38 +0530

    update

--- client/client.c
+++ client/client.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 25250
+#define SERVER_IP "172.18.0.2"
+
 
 
 #include <stdio.h>
@@ -25,31 +33,43 @@ void tostring(char str[], int num)
     str[len] = '\0';
 }
 
-int main(){
-  int clientSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
+  
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+      
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    /*send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    valread = read( sock , buffer, 1024);
+    printf("%s\n",buffer );*/
 
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(5432);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-
-  /*---- Connect the socket to the server using the address struct ----*/
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-
-  /*---- Read the message from the server into the buffer ----*/
+    /*---- Read the message from the server into the buffer ----*/
   int dogs=0,cats=0,cars=0,trucks=0;
   printf("Enter number of dogs:" );
   scanf("%d",&dogs);
@@ -61,21 +81,21 @@ int main(){
   scanf("%d",&trucks);
   printf("\n");
   tostring(buffer,dogs);
-  send(clientSocket,buffer,1024,0);
+  send(sock,buffer,1024,0);
   tostring(buffer,cats);
-  send(clientSocket,buffer,1024,0);
+  send(sock,buffer,1024,0);
   tostring(buffer,cars);
-  send(clientSocket,buffer,1024,0);
+  send(sock,buffer,1024,0);
   tostring(buffer,trucks);
-  send(clientSocket,buffer,1024,0);
+  send(sock,buffer,1024,0);
 
 
-  for(int i=1;i<=dogs+cats+cars+trucks;i++){    
+  for(int i=1;i<=dogs+cats+cars+trucks;i++){
     int size;
-    recv(clientSocket, buffer, 1024, 0);
+    recv(sock, buffer, 1024, 0);
     size=atoi(buffer);
     // printf("Picture Size=%d\n",size);
-    char *recv_buffer=(char*)(malloc(size*sizeof(char)));
+    char recv_buffer=(char)(malloc(size*sizeof(char)));
     // printf("Reading Picture Byte Array\n");
     // printf("Converting Byte Array to Picture\n");
     FILE *image;
@@ -84,7 +104,8 @@ int main(){
     char jp[]=".jpg";
     strcat(name,jp);
     image = fopen(name, "w");
-    recv(clientSocket, recv_buffer, size, 0);
+    //recv(sock, recv_buffer, size, 0);
+    read(sock, recv_buffer, size);
     // printf("Size of recv_buffer = %ld\n",sizeof(recv_buffer) );
     fwrite(recv_buffer,  1,size, image);
     fclose(image);
@@ -94,10 +115,13 @@ int main(){
   char n_images[50];
   tostring(n_images,dogs+cats+cars+trucks);
   strcat(com,n_images);
-  char f[20]=" > index.html";
+  char f[30]=" > index.html";
   strcat(com,f);
   system(com);
-  system("firefox index.html");
+	
 
-  return 0;
+
+
+
+    return 0;
 }
