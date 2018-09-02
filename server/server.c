7372cb08d680f0071a71cdc8d40dfcdbb7449709
iitkargonauts <42734813+iitkargonauts@users.noreply.github.com>
2018-09-02 23:50:43 +0530

    update

--- server/server.c
+++ server/server.c
@@ -1,8 +1,17 @@
-
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#define PORT 25250
+
+
+
+#include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
 
@@ -25,56 +34,62 @@ void tostring(char str[], int num)
 }
 
 
-int main(){
-  int welcomeSocket, newSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
 
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(5432);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-
-  /*---- Bind the address struct to the socket ----*/
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-
-  /*---- Listen on the socket, with 5 max connection requests queued ----*/
-  if(listen(welcomeSocket,5)==0)
-    printf("I'm listening\n");
-  else
-    printf("Error\n");
-
-  /*---- Accept call creates a new socket for the incoming connection ----*/
-  addr_size = sizeof serverStorage;
-  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-
-  /*---- Send message to the socket of the incoming connection ----*/
-
-
-  //Get Picture Size
-  
-  int dogs=0,cats=0,cars=0,trucks=0;
-  recv(newSocket, buffer, 1024, 0);
-  dogs=atoi(buffer);
-  recv(newSocket, buffer, 1024, 0);
-  cats=atoi(buffer);
-  recv(newSocket, buffer, 1024, 0);
-  cars=atoi(buffer);
-  recv(newSocket, buffer, 1024, 0);
-  trucks=atoi(buffer);
-
-  int a[4]={dogs,cats,cars,trucks};
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+      
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+      
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, 
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    int dogs=0,cats=0,cars=0,trucks=0;
+    valread = read( new_socket , buffer, 1024);
+    dogs=atoi(buffer);
+    valread = read( new_socket , buffer, 1024);
+    cats=atoi(buffer);
+    valread = read( new_socket , buffer, 1024);
+    cars=atoi(buffer);
+    valread = read( new_socket , buffer, 1024);
+    trucks=atoi(buffer);
+    int a[4]={dogs,cats,cars,trucks};
   char image[]="images/";
   char jp[]=".jpg";
   char index[4][2]={"1","2","3","4"};
@@ -83,12 +98,12 @@ int main(){
   // char ct[]="cats/";
   // char cr[]="cars/";
   // char t[]="trucks/";
-  
+
 
   for(int i=0;i<4;i++){
-    char location[25];   
+    char location[25];
     strcpy(location,image);
-    strcat(location,str[i]); 
+    strcat(location,str[i]);
     for(int j=0;j<a[i];j++){
       char final[40];
       strcpy(final,location);
@@ -104,20 +119,17 @@ int main(){
       size = ftell(picture);
       fseek(picture, 0, SEEK_SET);
       tostring(buffer,size);
-      send(newSocket,buffer,1024,0);
+      send(new_socket,buffer,1024,0);
       // printf("Sending Picture Size=%d\n",size);
       //Send Picture as Byte Array
       // printf("Sending Picture as Byte Array\n");
-      char *send_buffer=(char*)(malloc(size*sizeof(char)));
+      char send_buffer=(char)(malloc(size*sizeof(char)));
       fread(send_buffer, 1, size, picture);
       // printf("Size of send_buffer = %d\n",size );
-      send(newSocket,send_buffer,size,0);
+      send(new_socket,send_buffer,size,0);
       bzero(send_buffer, sizeof(send_buffer));
 
     }
   }
-
-  
-  
-  return 0;
+    return 0;
 }
