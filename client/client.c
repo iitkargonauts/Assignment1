4f25edcfd73d2940fb092919d2c9cb61d36ecd26
iitkargonauts <42734813+iitkargonauts@users.noreply.github.com>
2018-09-08 03:26:57 +0530

    done dana done dun

--- client/client.c
+++ client/client.c
@@ -1,10 +1,17 @@
+/* credit @Daniel Scocco */
+
+/****************** CLIENT CODE ****************/
+
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <math.h>
 #include <string.h>
-#define PORT 25250
-#define SERVER_IP "172.17.0.2"
+#include <arpa/inet.h>
+#define MAXSIZE 100000
+static char name[12];
+
 int isnum(char* str){
   int i;
   for(i=0;i<strlen(str);i++){
@@ -32,50 +39,28 @@ void tostring(char str[], int num)
     str[len] = '\0';
 }
 
-  
-int main(int argc, char const *argv[])
-{
-    struct sockaddr_in address;
-    int sock = 0, valread;
-    char useless[1024]="I am useless. Just to synchronise";
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1000] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-  
-    memset(&serv_addr, '0', sizeof(serv_addr));
-  
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-      
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr)<=0) 
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-  
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    /*send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );*/
+int main(int argc, char** argv){
+    system("rm -f *.jpg");
+    int clientSocket, num, b, p;
+    char fileb[1024];
+    int count[4] = {0};
+    int i[1] = {1};
+    char buffer[MAXSIZE];
+    struct sockaddr_in serverAddr;
+    socklen_t addr_size;
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(atoi(argv[2]));
+    serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    addr_size = sizeof serverAddr;
+    connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
 
-    /*---- Read the message from the server into the buffer ----*/
-  int dogs=0,cats=0,cars=0,trucks=0;
-  
-  // int dogs=0,cats=0,cars=0,trucks=0;
 
-  // size_t bufsize = 1024;
-  {char* str = (char*)malloc(1025*sizeof(char));
+    printf("Enter your Query here! :\n");
+    int dogs=0,cats=0,cars=0,trucks=0;
+  {
+    char* str = (char*)malloc(1025*sizeof(char));
     memset(str, '\0', 1024*sizeof(char));
   
     // printf("Enter a string : ");
@@ -121,66 +106,45 @@ int main(int argc, char const *argv[])
     }
     // printf("Recieved %d dogs, %d cats, %d trucks, %d cars", dogs, cats, trucks, cars);
   
-  
   }
+    count[0] = dogs;
+    count[1] = cats;
+    count[2] = cars;
+    count[3] = trucks;
+    int encode = dogs + cars*5 + cats*25 + trucks*125;
+    printf("%d %d %d %d\n",count[0], count[1], count[2], count[3]);
+    tostring(buffer, encode);
+    send(clientSocket, buffer, 1024, 0);
+    FILE* fp;
+    char str[4][10]={"dogs","cats","cars","trucks"};
+    for(int qw =0;qw<4;qw++){
+      for(int k = 0;k<count[qw];k++){
+        static char jp[6];
+        jp[0]=k+1-0+'0';
+        jp[1]='.';
+        jp[2]='j';
+        jp[3]='p';
+        jp[4]='g';
+        jp[5]='\0';
+        jp[6]='\0';
+        char final[50];
+        strcpy(final,str[qw]);
+        strcat(final,jp);
 
-
-  // printf("Enter number of dogs:" );
-  // scanf("%d",&dogs);
-  // printf("\nEnter number of cats:" );
-  // scanf("%d",&cats);
-  // printf("\nEnter number of cars:" );
-  // scanf("%d",&cars);
-  // printf("\nEnter number of trucks:" );
-  // scanf("%d",&trucks);
-  // printf("\n");
-  tostring(buffer,dogs);
-  send(sock,buffer,1024,0);
-  tostring(buffer,cats);
-  send(sock,buffer,1024,0);
-  tostring(buffer,cars);
-  send(sock,buffer,1024,0);
-  tostring(buffer,trucks);
-  send(sock,buffer,1024,0);
-
-  // printf("abc\n");
-
-  for(int i=1;i<=dogs+cats+cars+trucks;i++){
-    int size;
-    read(sock, buffer, 1024);
-    size=atoi(buffer);
-    printf("Picture Size=%d\n",size);
-    char* recv_buffer=(char*)(malloc(size*sizeof(char)));
-    // printf("Reading Picture Byte Array\n");
-    // printf("Converting Byte Array to Picture\n");
-    FILE *image;
-    char name[20];
-    tostring(name,i);
-    char jp[]=".jpg";
-    strcat(name,jp);
-    image = fopen(name, "w");
-    //recv(sock, recv_buffer, size, 0);
-    read(sock, recv_buffer, size);
-    // printf("Size of recv_buffer = %ld\n",sizeof(recv_buffer) );
-    fwrite(recv_buffer,  1,size, image);
-    fclose(image);
-    send(sock,useless,1024,0);  
-    free(recv_buffer);
+        fp = fopen(final, "wb");
+        b = recv(clientSocket, buffer, sizeof(buffer), 0);
+        printf("%d\n",b);
+        fwrite(buffer, 1, b, fp);
+        // printf("here_11\n");
+      
+        send(clientSocket, i, sizeof(int), 0);
+        // printf("here_12\n");
+      
+        fclose(fp);
+      }
   }
+  system("./web.sh > index.html");
+  system("firefox index.html  ");
 
-  // printf("abc\n");
-
-  char com[50]="./web.sh ";
-  char n_images[50];
-  tostring(n_images,dogs+cats+cars+trucks);
-  strcat(com,n_images);
-  char f[30]=" > index.html";
-  strcat(com,f);
-  system(com);
-	
-  system("firefox index.html");
-
-
-
-    return 0;
+  return 0;
 }
